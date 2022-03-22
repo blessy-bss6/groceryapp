edfff80ab4c3c9d14244ad959edd7b9ac4e69cf8
unknown <Administrator@BSS.LOCAL>
2022-03-22 18:17:12 +0530

    update page

--- lib/offWork/selCategoryScr.dart
+++ lib/offWork/selCategoryScr.dart
@@ -9,72 +9,56 @@ class SelCategoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: BaseAppBar(
-        boolLeading: true,
-        centerTitle: true,
-        title: 'Category',
-      ),
-
-      // body: CustomScrollView(
-      //   slivers: [
-
-      //   ],
-      // ),
-      body: Container(
-        child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: 3,
-            itemBuilder: ((context, index) => ListView(
-                  shrinkWrap: true,
-                  children: [
-                    Text(
-                      'Category name',
-                      style: labelTextStyle,
-                    ),
-                    Wrap(
-                      children: List.generate(
-                        10,
-                        (index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: FilterChip(
-                              label: Text('index $index'),
-                              onSelected: (val) {},
-                              // disabledColor :
-                            ),
-                          );
-                        },
-                      ),
-                    )
-                  ],
-                ))),
-      ),
-    );
-  }
-}
-
-class StackOver extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(
-          top: 60,
-          left: 10.0,
-          right: 10.0,
+        appBar: BaseAppBar(
+          boolLeading: true,
+          centerTitle: true,
+          title: 'Category',
         ),
-        child: Wrap(children: [
-          ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return FilterChip(
-                label: Text('index $index'),
-                onSelected: (val) {},
-              );
-            },
+
+        // body: CustomScrollView(
+        //   slivers: [
+
+        //   ],
+        // ),
+        body: Container(
+          child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 3,
+              itemBuilder: ((context, index) => ListView(
+                    shrinkWrap: true,
+                    children: [
+                      Text(
+                        'Category name',
+                        style: labelTextStyle,
+                      ),
+                      Wrap(
+                        children: List.generate(
+                          10,
+                          (index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: FilterChip(
+                                label: Text('index $index'),
+                                onSelected: (val) {},
+                                // disabledColor :
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ))),
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 3.0),
+          child: Btn(
+            onTap: () {},
+            height: 35,
+            width: 200,
+            btnName: 'Submit',
+            color: Colors.green,
+            txtColor: offWhiteColor,
           ),
-        ]),
-      ),
-    );
+        ));
   }
 }
